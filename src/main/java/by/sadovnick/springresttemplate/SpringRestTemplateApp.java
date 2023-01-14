d06119aef115ffef41747eff0ccd4ec9d12d1a70
Sadonikov Alexander <alecos100@gmail.com>
2023-01-14 23:18:55 +0300

    first commit

---
+++ src/main/java/by/sadovnick/springresttemplate/SpringRestTemplateApp.java
@@ -0,0 +1,13 @@
+package by.sadovnick.springresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTemplateApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestTemplateApp.class, args);
+	}
+
+}
---
+++ src/test/java/by/sadovnick/springresttemplate/Pp316ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.sadovnick.springresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp316ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
